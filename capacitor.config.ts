c3fa7f7e30c8a3cb453eb0fd13813ec0e8121666
Mahrez <mahz@live.fr>
2025-11-22 00:48:20 +0100

    fix ios simulation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.c0ef37b8e9934782b45fd623824039bb',
+  appId: 'com.taffaran.app',
   appName: 'Taffaran',
   webDir: 'dist',
-  server: {
-    url: 'https://c0ef37b8-e993-4782-b45f-d623824039bb.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   plugins: {
     Haptics: {
       enabled: true
